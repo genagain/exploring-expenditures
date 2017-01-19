c780b7a6d1144a61add57f0cb566cc445268438a
genagain <ohta.g@husky.neu.edu>
2017-01-18 22:45:01 -0500

    fixed ordering of months

--- monthly_spending.py
+++ monthly_spending.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import calendar
 import ipdb
 
 def monthly_total(month):
    idx = withdrawals["Month"] == month
    return (month, abs(withdrawals[idx]["Amount"].sum()))
+
+def to_month_name(integer):
+    month_mapping = {k: v for k,v in enumerate(calendar.month_name)}
+    return month_mapping[integer]
 
 data = pd.DataFrame.from_csv('clean_transactions.csv')
 
@@ -13,10 +18,10 @@ idx = data['Amount'] < 0
 withdrawals = data[idx]
 
 months = withdrawals.index.month
-month_labels = withdrawals.index.strftime('%B')
 withdrawals.loc[:,('Month')] = months
+
 uniq_months = np.unique(months)
-uniq_month_labels = np.unique(month_labels)
+uniq_month_labels = map(lambda m: to_month_name(m), uniq_months)
 
 monthly_totals = map(lambda m: monthly_total(m), uniq_months)
 monthly_totals_df = pd.DataFrame(monthly_totals, columns=['Month', 'Total'])
