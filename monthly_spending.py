395e158eabda38b128d2adfc1c464dc964e744ae
genagain <ohta.g@husky.neu.edu>
2017-01-20 12:09:46 -0500

    refactoring int to month map function

--- monthly_spending.py
+++ monthly_spending.py
@@ -8,10 +8,6 @@ def monthly_total(month):
    idx = withdrawals["Month"] == month
    return (month, abs(withdrawals[idx]["Amount"].sum()))
 
-def to_month_name(integer):
-    month_mapping = {k: v for k,v in enumerate(calendar.month_name)}
-    return month_mapping[integer]
-
 data = pd.DataFrame.from_csv('clean_transactions.csv')
 
 idx = data['Amount'] < 0
@@ -21,19 +17,17 @@ months = withdrawals.index.month
 withdrawals.loc[:,('Month')] = months
 
 uniq_months = np.unique(months)
-uniq_month_labels = map(lambda m: to_month_name(m), uniq_months)
+month_mapping = {k: v for k,v in enumerate(calendar.month_name)}
+uniq_month_labels = map(lambda m: month_mapping[m], uniq_months)
 
-monthly_totals = map(lambda m: monthly_total(m), uniq_months)
+monthly_totals = map(monthly_total, uniq_months)
 monthly_totals_df = pd.DataFrame(monthly_totals, columns=['Month', 'Total'])
 
 fig = plt.figure()
-
 ax = fig.add_subplot(111)
-
 plt.bar(monthly_totals_df['Month'], monthly_totals_df['Total'], align='center')
 plt.xticks(monthly_totals_df['Month'], uniq_month_labels)
 ax.set_title('Withdrawals by month')
 ax.set_xlabel('Months')
 ax.set_ylabel('Withdrawals ($)')
-
 plt.show()
