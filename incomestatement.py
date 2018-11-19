786e299c182567471f412ae9893de1b6d23e9dd7
genagain <ohta.g@husky.neu.edu>
2018-11-19 15:30:40 -0500

    wip created a way to select all of the aggregated records

--- incomestatement.py
+++ incomestatement.py
@@ -4,24 +4,33 @@ import numpy as np
 import pandas as pd
 from config import OPTIMAL_BREAKDOWN
 
-def get_income(transactions):
+def sum_amounts(transactions, return_selected=False):
+    if return_selected:
+      return transactions
+    else:
+      total_amount = transactions.amount.sum()
+      return np.round(total_amount, 2)
+
+def get_income(transactions, return_selected=False):
     idx = transactions.category == 'paycheck'
-    income = transactions[idx].amount.sum()
-    return np.round(income, 2)
+    return sum_amounts(transactions[idx], return_selected)
 
-def get_transportation_expenses(transactions):
+def get_transportation_expenses(transactions, return_selected=False):
     idx = transactions.original_description.str.contains('LYFT')
-    expenses = transactions[idx].amount.sum()
-    return np.round(expenses, 2)
+    return sum_amounts(transactions[idx], return_selected)
 
-def get_net_qapital_savings(transactions):
+def get_utilities(transactions, return_selected=False):
+    idx = (transactions.account_name == 'Bills') & (transactions.original_description.str.contains('COMCAST|EVERSOURCE'))
+    return sum_amounts(transactions[idx], return_selected)
+
+def get_net_qapital_savings(transactions, return_selected=False):
     withdrawal_idx = transactions.original_description.str.contains('QAPITAL') & transactions.category.isin(['savings', 'deposit'])
-    withdrawals = transactions[withdrawal_idx].amount.sum()
+    withdrawals = sum_amounts(transactions[withdrawal_idx], return_selected)
     deposits_idx = transactions.original_description.str.contains('QAPITAL') & transactions.category.isin(['income', 'transfer'])
-    deposits = transactions[deposits_idx].amount.sum()
-    return np.round(withdrawals - deposits)
+    deposits = sum_amounts(transactions[deposits_idx], return_selected)
+    return (withdrawals, deposits) if return_selected else np.round(withdrawals - deposits)
 
-def get_net_qapital_breakdown(transactions):
+def get_net_qapital_breakdown(transactions, return_selected=False):
     residual_savings = get_net_qapital_savings(transactions)
 
     breakdown = OrderedDict([ (category, 0) for category in OPTIMAL_BREAKDOWN.keys() ])
@@ -35,47 +44,38 @@ def get_net_qapital_breakdown(transactions):
 
     return breakdown
 
-def get_investments(transactions):
+def get_investments(transactions, return_selected=False):
     qapital_investments= get_net_qapital_breakdown(transactions)['investments']
     vanguard_idx = transactions.original_description.str.contains('VANGUARD')
-    vanguard_savings = transactions[vanguard_idx].amount.sum()
+    vanguard_savings = sum_amounts(transactions[vanguard_idx], return_selected)
     business_idx = (transactions.original_description.str.contains('Online scheduled transfer from CHK 4604')) & (transactions.account_name == 'Business Fundamentals Chk')
-    business_investment = transactions[business_idx].amount.sum()
-    return np.round(qapital_investments+ vanguard_savings + business_investment)
+    business_investment = sum_amounts(transactions[business_idx], return_selected)
+    return (vanguard_savings, business_investment) if return_selected else np.round(qapital_investments+ vanguard_savings + business_investment)
 
-def get_credit_card_expenses(transactions):
+def get_credit_card_expenses(transactions, return_selected=False):
     idx = transactions.account_name == 'nRewards Visa'
-    expenses = transactions[idx].amount.sum()
-    return np.round(expenses, 2)
+    return sum_amounts(transactions[idx], return_selected)
 
-def get_net_venmo(transactions):
+def get_net_venmo(transactions, return_selected=False):
     withdrawal_idx = transactions.original_description.str.contains('VENMO DES:PAYMENT')
-    withdrawals = transactions[withdrawal_idx].amount.sum()
+    withdrawals = sum_amounts(transactions[withdrawal_idx], return_selected)
     deposits_idx = transactions.original_description.str.contains('VENMO DES:CASHOUT')
-    deposits = transactions[deposits_idx].amount.sum()
-    return np.round(deposits - withdrawals)
+    deposits = sum_amounts(transactions[deposits_idx], return_selected)
+    return (deposits, withdrawals) if return_selected else np.round(deposits - withdrawals)
 
-def get_utilities(transactions):
-    idx = (transactions.account_name == 'Bills') & (transactions.original_description.str.contains('COMCAST|EVERSOURCE'))
-    expenses = transactions[idx].amount.sum()
-    return np.round(expenses, 2)
-
-def get_rent(transactions):
+def get_rent(transactions, return_selected=False):
     idx = transactions.original_description.str.contains('(?i)willow')
-    rent = transactions[idx].amount.sum()
-    return np.round(rent, 2)
+    return sum_amounts(transactions[idx], return_selected)
 
-def get_phone_bill(transactions):
+def get_phone_bill(transactions, return_selected=False):
     idx = transactions.original_description.str.contains('(?i)project fi')
-    phone_bill = transactions[idx].amount.sum()
-    return np.round(phone_bill, 2)
+    return sum_amounts(transactions[idx], return_selected)
 
-def get_groceries(transactions):
+def get_groceries(transactions, return_selected=False):
     idx = transactions.original_description.str.contains('(?i)instacart|star market')
-    groceries = transactions[idx].amount.sum()
-    return np.round(groceries, 2)
+    return sum_amounts(transactions[idx], return_selected)
 
-def get_fixed_costs(transactions):
+def get_fixed_costs(transactions, return_selected=False):
     rent = get_rent(transactions)
     utilities = get_utilities(transactions)
     phone_bill = get_phone_bill(transactions)
@@ -84,6 +84,6 @@ def get_fixed_costs(transactions):
 
     return rent + utilities + phone_bill + groceries + breakdown['fixed_costs']
 
-def get_savings_goals(transactions):
+def get_savings_goals(transactions, return_selected=False):
     breakdown = get_net_qapital_breakdown(transactions)
     return breakdown['savings_goals']
--- tests/test_incomestatement.py
+++ tests/test_incomestatement.py
@@ -17,11 +17,22 @@ def test_get_income(test_transactions):
   assert incomestatement.get_income(august) == 5516.76
   assert incomestatement.get_income(september) == 4789.53
 
+def test_get_income_returned_selected(test_transactions):
+  for month in test_transactions:
+    selected_records = incomestatement.get_income(month, return_selected=True)
+    assert selected_records.category.unique() == ['paycheck']
+
 def test_get_transportation_expenses(test_transactions):
   july, august, september = test_transactions
   assert incomestatement.get_transportation_expenses(july) == 512.0
   assert incomestatement.get_transportation_expenses(august) == 651.0
   assert incomestatement.get_transportation_expenses(september) == 558.0
+
+def test_get_transportation_expenses_return_selected(test_transactions):
+  for month in test_transactions:
+    selected_records = incomestatement.get_transportation_expenses(month, return_selected=True)
+    descriptions = selected_records.original_description.unique()
+    assert all(['LYFT' in description for description in descriptions])
 
 def test_get_utilities(test_transactions):
   july, august, september = test_transactions
@@ -29,11 +40,26 @@ def test_get_utilities(test_transactions):
   assert incomestatement.get_utilities(august) == 161.95
   assert incomestatement.get_utilities(september) == 67.03
 
+def test_get_utilities_return_selected(test_transactions):
+  for month in test_transactions:
+    selected_records = incomestatement.get_utilities(month, return_selected=True)
+    descriptions = selected_records.original_description
+    assert all(['COMCAST' in description or 'EVERSOURCE' in description for description in descriptions])
+
 def test_get_net_qapital_savings(test_transactions):
   july, august, september = test_transactions
   assert incomestatement.get_net_qapital_savings(july) == 1915.0
   assert incomestatement.get_net_qapital_savings(august) == 1029.0
   assert incomestatement.get_net_qapital_savings(september) == -482.0
+
+def test_get_net_qapital_savings_return_selected(test_transactions):
+  july, august, september = test_transactions
+  for month in test_transactions:
+    withdrawals, deposits = incomestatement.get_net_qapital_savings(month, return_selected=True)
+    assert [category in ['savings', 'deposit'] for category in withdrawals.category.unique()]
+    assert [category in ['income', 'transfer'] for category in deposits.category.unique()]
+    assert all(['QAPITAL' in description for description in withdrawals.original_description.unique()])
+    assert all(['QAPITAL' in description for description in deposits.original_description.unique()])
 
 # TODO similate different investment scenarios bad, optimal, exceptional
 
@@ -57,16 +83,36 @@ def test_get_net_venmo(test_transactions):
   assert incomestatement.get_net_venmo(august) == 904.0
   assert incomestatement.get_net_venmo(september) == 1033.0
 
+def test_get_net_venmo_return_selected(test_transactions):
+  july, august, september = test_transactions
+  for month in test_transactions:
+    deposits, withdrawals = incomestatement.get_net_venmo(month, return_selected=True)
+    assert all(['VENMO DES:PAYMENT' in description for description in withdrawals.original_description.unique()])
+    assert all(['VENMO DES:CASHOUT' in description for description in deposits.original_description.unique()])
+
 def test_get_credit_card_expenses(test_transactions):
   july, august, september = test_transactions
   assert incomestatement.get_credit_card_expenses(july) == 1836.74
   assert incomestatement.get_credit_card_expenses(august) == 2056.28
   assert incomestatement.get_credit_card_expenses(september) == 2210.19
 
+def test_get_credit_card_expenses_return_selected(test_transactions):
+  for month in test_transactions:
+    selected_records = incomestatement.get_credit_card_expenses(month, return_selected=True)
+    accounts = selected_records.account_name.unique()
+    assert ['nRewards Visa'] == accounts
+
 def test_get_rent(test_transactions):
-  july, august, september = test_transactions
+  _, august, september = test_transactions
   assert incomestatement.get_rent(august) == 2500.00
   assert incomestatement.get_rent(september) == 2600.00
+
+def test_get_rent_return_selected(test_transactions):
+  _, august, september = test_transactions
+  for month in (august, september):
+    selected_records = incomestatement.get_rent(month, return_selected=True)
+    descriptions = selected_records.original_description
+    assert all(['willow' in description.lower() for description in descriptions])
 
 def test_get_phone_bill(test_transactions):
   july, august, september = test_transactions
@@ -74,11 +120,23 @@ def test_get_phone_bill(test_transactions):
   assert incomestatement.get_phone_bill(august) == 79.11
   assert incomestatement.get_phone_bill(september) == 87.35
 
+def test_get_phone_bill_return_selected(test_transactions):
+  for month in test_transactions:
+    selected_records = incomestatement.get_phone_bill(month, return_selected=True)
+    descriptions = selected_records.original_description
+    assert all(['project fi' in description.lower() for description in descriptions])
+
 def test_get_groceries(test_transactions):
   july, august, september = test_transactions
   assert incomestatement.get_groceries(july) == 173.98
   assert incomestatement.get_groceries(august) == 122.43
   assert incomestatement.get_groceries(september) == 39.60
+
+def test_get_groceries_return_selected(test_transactions):
+  for month in test_transactions:
+    selected_records = incomestatement.get_groceries(month, return_selected=True)
+    descriptions = selected_records.original_description
+    assert all(['INSTACART' in description or 'STAR MARKET' in description for description in descriptions])
 
 def test_fixed_costs(test_transactions):
   july, august, september = test_transactions
@@ -104,6 +162,13 @@ def test_get_investments(test_transactions):
   assert incomestatement.get_investments(july) == 525
   assert incomestatement.get_investments(august) == 550
   assert incomestatement.get_investments(september) == 300
+
+def test_get_investments_select_returned(test_transactions):
+  for month in test_transactions:
+    vanguard_savings, business_investment = incomestatement.get_investments(month, return_selected=True)
+    assert all(['VANGUARD' in description for description in vanguard_savings.original_description])
+    assert all(['Online scheduled transfer from CHK 4604' in description for description in business_investment.original_description])
+    assert ['Business Fundamentals Chk'] == business_investment.account_name.unique()
 
 def test_get_savings_goals(test_transactions):
   july, august, september = test_transactions
