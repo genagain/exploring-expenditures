d6ead05da84488ff54bae51d589c5c0ac3337f1e
genagain <ohta.g@husky.neu.edu>
2018-12-22 14:41:08 -0500

    add curology

--- incomestatement.py
+++ incomestatement.py
@@ -92,6 +92,10 @@ def get_audible(transactions, return_selected=False):
     idx = transactions.original_description.str.contains('(?i)audible')
     return sum_amounts(transactions[idx], return_selected)
 
+def get_curology(transactions, return_selected=False):
+    idx = transactions.original_description.str.contains('(?i)curology')
+    return sum_amounts(transactions[idx], return_selected)
+
 def get_debitize_payments(transactions, return_selected=False):
     idx = transactions.original_description.str.contains('(?i)debitize|mail remittance payment received|ach payment received')
     return sum_amounts(transactions[idx], return_selected)
--- tests/test_incomestatement.py
+++ tests/test_incomestatement.py
@@ -161,6 +161,12 @@ def test_get_audible(test_transactions):
   assert incomestatement.get_audible(august) == 14.95
   assert incomestatement.get_audible(september) == 14.95
 
+def test_get_curology(test_transactions):
+  july, august, september = test_transactions
+  assert incomestatement.get_curology(july) == 0
+  assert incomestatement.get_curology(august) == 0
+  assert incomestatement.get_curology(september) == 0
+
 def test_get_groceries_return_selected(test_transactions):
   for month in test_transactions:
     selected_records = incomestatement.get_groceries(month, return_selected=True)
