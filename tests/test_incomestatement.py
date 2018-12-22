e2aef480a71e68fa6952cb7d43fc42507263b990
genagain <ohta.g@husky.neu.edu>
2018-12-22 11:29:02 -0500

    delete commented out test

--- tests/test_incomestatement.py
+++ tests/test_incomestatement.py
@@ -143,12 +143,6 @@ def test_get_groceries(test_transactions):
   assert incomestatement.get_groceries(august) == 122.43
   assert incomestatement.get_groceries(september) == 218.22
 
-# def test_get_groceries(test_transactions):
-  # july, august, september = test_transactions
-  # assert incomestatement.get_groceries(july) == 271.04
-  # assert incomestatement.get_groceries(august) == 122.43
-  # assert incomestatement.get_groceries(september) == 218.22
-
 def test_get_groceries_return_selected(test_transactions):
   for month in test_transactions:
     selected_records = incomestatement.get_groceries(month, return_selected=True)
